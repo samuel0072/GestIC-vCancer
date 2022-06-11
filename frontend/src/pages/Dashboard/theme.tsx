2b7ed2c6ea60de20b4df48ce1aefab822c09d7b0
Mateus Felismino <mffsp@ic.ufal.br>
2022-06-10 21:21:22 -0300

    frontend: initial desktop dark mode implementation

---
+++ frontend/src/pages/Dashboard/theme.tsx
@@ -0,0 +1,36 @@
+export const DarkBox = {
+    background: "#121212",
+    color: "teal",
+}
+
+
+export const LightBox_1 = {
+    background: "teal",
+    borderColor: "white",
+    padding: "16px",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "2px"
+
+}
+
+export const LightBox_2 = {
+    padding: "16px",
+    display: "flex",
+    background: "#f8f8ff",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "2px"
+
+}
+
+export const DarkBox_1 = {
+    padding: "16px",
+    display: "flex",
+    background: "#121212",
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: "6px"
+
+}
